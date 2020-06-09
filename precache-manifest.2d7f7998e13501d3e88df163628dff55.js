9d579fd9328b5983419ad70f429cf2eb346f8f66
Sukriti Bhardwaj <sukriti@lawn-128-61-48-150.lawn.gatech.edu>
2020-06-09 19:39:46 -0400

    Updates

---
+++ precache-manifest.2d7f7998e13501d3e88df163628dff55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82554a217b662f6f7e0cb3405d8957e0",
+    "url": "/por/index.html"
+  },
+  {
+    "revision": "ceec1533f5b1bdb4f9c7",
+    "url": "/por/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "842f8023218b34c632fd",
+    "url": "/por/static/css/main.2b29259e.chunk.css"
+  },
+  {
+    "revision": "ceec1533f5b1bdb4f9c7",
+    "url": "/por/static/js/2.b0e192fb.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/por/static/js/2.b0e192fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "842f8023218b34c632fd",
+    "url": "/por/static/js/main.91a29acc.chunk.js"
+  },
+  {
+    "revision": "b4410242a4f4211fbcf9",
+    "url": "/por/static/js/runtime-main.3a8a0079.js"
+  }
+]);
